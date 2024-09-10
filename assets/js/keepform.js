a5c161762e5f6bbcb421ddf7e14f771ddcac249b
Pascal Mairot <168566181+PA-MAI@users.noreply.github.com>
2024-09-10 15:45:50 +0200

    debug result on radio button for keepform

--- assets/js/keepform.js
+++ assets/js/keepform.js
@@ -6,11 +6,36 @@ export function keepFormData(form) {
     // get all form fields 
     const inputs = form.querySelectorAll("input, select, textarea");
 
+    // object to track the state of radio buttons
+    const radioGroups = {}; 
+
     inputs.forEach(input => {
-        formData[input.name] = input.value; // Stocke value
+
+        if (input.type === 'checkbox') {
+            // for checkbox, check data
+            formData[input.name] = input.checked ? 'on' : 'off';
+
+        } else if (input.type === 'radio') {
+            // for radios, check data
+            if (!radioGroups[input.name]) {
+                radioGroups[input.name] = 'off'; // Init data to'off'
+            }
+            if (input.checked) {
+                radioGroups[input.name] = input.value;
+            }
+        } else {
+            if (input.name) { 
+            formData[input.name] = input.value;
+            }
+        }
+
     });
 
-    // push new data in table formDataHistory
+// push radios data on formData
+Object.keys(radioGroups).forEach(groupName => {
+    formData[groupName] = radioGroups[groupName];
+});
+ // push new data in table formDataHistory
     formDataHistory.push(formData);
     console.log("Données du formulaire sauvegardées :", formData);
 }
